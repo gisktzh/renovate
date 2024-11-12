89f37cf386896f0576cc58b3863a486c4863cb9d
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2024-11-12 15:02:03 +0100

    Temporary removed all except the web UI repositories.

--- renovate-config.js
+++ renovate-config.js
@@ -3,9 +3,9 @@ module.exports = {
     // add your own project to the following array in the syntax 'PROJECT_NAME/REPOSITORY_NAME'
     repositories: [
         'gisktzh/gb3-web_ui',
-        'gisktzh/gb3-monitoring',
-        'gisktzh/gb3-search_elastic',
-        'gisktzh/gb3-cms',
-        'gisktzh/gb3-testcases',
+        // 'gisktzh/gb3-monitoring',
+        // 'gisktzh/gb3-search_elastic',
+        // 'gisktzh/gb3-cms',
+        // 'gisktzh/gb3-testcases',
     ],
 };
